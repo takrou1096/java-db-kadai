dafecfba2d2667321676c680a59a99f61791e6da
takurou Yonekura <takurou11009966@gmail.com>
2025-03-31 21:13:11 +0900

    更	新

--- java_study/src/kadai_021/DictionatyExec_Chapter21.java
+++ java_study/src/kadai_021/DictionatyExec_Chapter21.java
@@ -1,5 +1,9 @@
 package kadai_021;
 
 public class DictionatyExec_Chapter21 {
-
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();	
+		String[] wordsToSearch = {"apple", "banana", "grape", "orange"};
+		dictionary.referer(wordsToSearch);
+	}
 }
