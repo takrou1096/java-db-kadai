202983ff6f1c78423ab5fdb3e7cd96a88b3a4fcc
takurou Yonekura <takurou11009966@gmail.com>
2025-04-13 14:18:21 +0900

    更新

--- java_study/src/kadai_028/JyankenExec_Chapter28.java
+++ java_study/src/kadai_028/JyankenExec_Chapter28.java
@@ -1,5 +1,15 @@
 package kadai_028;
 
-public class JyankenExec_Chapter28 {
+import java.util.Scanner;
 
+public class JyankenExec_Chapter28 {
+	public static void main(String[] args) {
+        // Janken クラスのインスタンスを作成
+		Jyanken_Chapter28 janken = new Jyanken_Chapter28();
+		Scanner scanner = new Scanner(System.in);
+		String user = janken.getMyChoice();
+		String oppnent = janken.getRandom();
+		janken.playGame(user, oppnent);
+		scanner.close();
+	}
 }
--- java_study/src/kadai_028/Jyanken_Chapter28.java
+++ java_study/src/kadai_028/Jyanken_Chapter28.java
@@ -1,21 +1,24 @@
 package kadai_028;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Jyanken_Chapter28 {
 	public String getMyChoice() {
-		Scanner scanner = new Scanner(System.in);
-		while(true) {
-			System.out.println("ジャンケンの手を入力してください。");
-			String input = scanner.next();
+			System.out.println("自分のじゃんけんの手を入力しましょう");
+			System.out.println("グーはrockのrを入力しましょう");
+			System.out.println("チョキはscissorsのsを入力しましょう");
+			System.out.println("パーはpaperのpを入力しましょう");
+			
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
 			if(!input.equals("r")||!input.equals("s")||!input.equals("p")){	
-				System.out.println("r,s,pで入力してください。");
-			    continue;
+				System.out.println(input);
 			}else{
-				scanner.close();
-				return input;
+				System.out.println("r,s,pで入力してください。");
 			}
+			scanner.close();
+			return input;
 		}
-	}
 	
 	public String getRandom() {
 		String[] jankenArray = {"r" ,"s" ,"p"};
@@ -23,7 +26,20 @@ public class Jyanken_Chapter28 {
 		return jankenArray[randomIndex];
 	}
 	
-	public String playGame() {
+	public void playGame(String myChoice ,String random) {
+		//自分と相手の手を出力
+		HashMap<String ,String> janken = new HashMap<>();
+		janken.put("r" ,"グー");
+		janken.put("s" ,"チョキ");
+		janken.put("p" ,"パー");
+		System.out.println("自分の手は" + janken.get(myChoice) +"対戦相手の手は" + janken.get(random));
 		
+		if((myChoice.equals("r") && random.equals("s")) || (myChoice.equals("s") && random.equals("p")) || (myChoice.equals("p")&&random.equals("r"))) {
+			System.out.println("自分の勝ちです"); 	
+		}else if((myChoice.equals("r") && random.equals("p")) || (myChoice.equals("s") && random.equals("r")) || (myChoice.equals("p")&&random.equals("s"))){
+			System.out.println("自分の負けです");	
+		}else {
+			System.out.println("あいこです");
+		}
 	}
 }
